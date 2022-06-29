fbae65f788992d0101f82927b359b5b3415fc4bc
nkommu <nagaraju.kommu>
2022-06-30 00:37:44 +0530

    added breakstmt.py file to local repo

---
+++ breakstmt.py
@@ -0,0 +1,11 @@
+while True:
+    msg=input("> ").lower()
+    
+    if msg[0]=="#":
+        continue
+    
+    print(msg)
+    if msg=="quit":
+        print("break the flow")
+        break
+    print("done")
